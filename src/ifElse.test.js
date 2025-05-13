e47ce6c901a8ed218dbff75c00ee994a16c9cfa0
Alenalenk <alena.lenk.worku@gmail.com>
2025-05-13 10:59:16 +0300

    add tests

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,31 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
-
+  it(`should be declared`, () => {
+    expect(ifElse).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  test('should returns correct result if condition return true', () => {
+    const condition = jest.fn(() => true);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+    expect(condition).toHaveBeenCalled();
+    expect(first).toHaveBeenCalled();
+    expect(second).not.toHaveBeenCalled();
+  });
+
+  test('should returns correct result if condition return false', () => {
+    const condition = jest.fn(() => false);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+    expect(condition).toHaveBeenCalled();
+    expect(first).not.toHaveBeenCalled();
+    expect(second).toHaveBeenCalled();
+  });
 });
